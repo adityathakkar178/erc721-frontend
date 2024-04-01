34cca855c12d7904477fffbef9acb4bd111d2690
adityathakkar178 <160093854+adityathakkar178@users.noreply.github.com>
2024-04-01 09:46:03 +0530

    nft-style-fixed

--- src/Components/NFT.js
+++ src/Components/NFT.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import Card from 'react-bootstrap/Card';
+import { Container, Card, Col, Row } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const NFT = () => {
@@ -18,40 +18,42 @@ const NFT = () => {
     }, []);
 
     return (
-        <div>
+        <>
             <h3 className="text-center mt-4 mb-4">Token List</h3>
-            <div className="row">
-                {tokens.map((token) => (
-                    <div className="col-md-4 mb-4" key={token._id}>
-                        <Card className="border-0">
-                            <div className="text-center">
-                                <Card.Img
-                                    variant="top"
-                                    src={`http://localhost:3004/uploads/${token.image}`}
-                                    alt={token.name}
-                                    style={{
-                                        height: '200px',
-                                        width: '200px',
-                                        objectFit: 'cover',
-                                    }}
-                                />
-                            </div>
-                            <Card.Body>
-                                <Card.Title className="text-center mb-2">
-                                    <strong>Name: {token.name}</strong>
-                                </Card.Title>
-                                <Card.Text className="text-center mb-2">
-                                    URI: {token.uri}
-                                </Card.Text>
-                                <Card.Text className="text-center mb-2">
-                                    Address: {token.address}
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </div>
-                ))}
-            </div>
-        </div>
+            <Container fluid='xl'>
+                <Row className="justify-content-center">
+                    {tokens.map((token) => (
+                        <Col key={token._id}>
+                            <Card className="border-0">
+                                <div className="text-center">
+                                    <Card.Img
+                                        variant="top"
+                                        src={`http://localhost:3004/uploads/${token.image}`}
+                                        alt={token.name}
+                                        style={{
+                                            height: '200px',
+                                            width: '200px',
+                                            objectFit: 'cover',
+                                        }}
+                                    />
+                                </div>
+                                <Card.Body>
+                                    <Card.Title className="text-center mb-2">
+                                        <strong>Name: {token.name}</strong>
+                                    </Card.Title>
+                                    <Card.Text className="text-center mb-2">
+                                        URI: {token.uri}
+                                    </Card.Text>
+                                    <Card.Text className="text-center mb-2">
+                                        Address: {token.address}
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
+        </>
     );
 };
 
